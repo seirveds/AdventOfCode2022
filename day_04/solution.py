7aa080ce5afd45a3f3edc16a60fc050d3182a5c7
Sidney de Vries <sidney.de.vries96@gmail.com>
2022-12-06 19:14:58 +0100

    Finished day 4 part 2

--- day_04/solution.py
+++ day_04/solution.py
@@ -6,5 +6,13 @@ with open("day_04/input.txt", "r") as f:
 # Part 1 #
 ##########
 
+# In how many assignment pairs does one range fully contain the other?
+#                      right pair contained in left pair                      left pair contained in right pair
 print(f"Part 1: {sum([(ids[0][0] <= ids[1][0] and ids[0][1] >= ids[1][1]) or (ids[0][0] >= ids[1][0] and ids[0][1] <= ids[1][1]) for ids in input_])}")
 
+##########
+# Part 2 #
+##########
+
+# In how many assignment pairs do the ranges overlap?
+print(f"Part 2: {sum([(group[1][0] <= group[0][1] and group[1][0] >= group[0][0]) or (group[0][0] <= group[1][1] and group[0][0] >= group[1][0]) for group in input_])}")
