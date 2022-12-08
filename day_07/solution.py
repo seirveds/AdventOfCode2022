aa08c94b3186fb678edded38161599a8d4bfe440
Sidney de Vries <sidney.de.vries96@gmail.com>
2022-12-08 20:16:21 +0100

    Finished day 7 part 2

--- day_07/solution.py
+++ day_07/solution.py
@@ -23,9 +23,17 @@ for line in command_log:
         # Update size of directory and all parent directories
         for i in range(len(curr_dir)):
             dir_sizes["".join(curr_dir[:i + 1])] += int(size)
-            
+
+# Find all of the directories with a total size of at most 100000.
+# What is the sum of the total sizes of those directories?         
 print(f"Part 1: {sum([v for v in dir_sizes.values() if v <= 100000])}")
 
 ##########
 # Part 2 #
 ##########
+
+# Find the smallest directory that, if deleted, would free up enough space on the filesystem
+# to run the update. What is the total size of that directory?
+
+# Smallest directory with a size larger than what is needed to free up more than 30.000.000 storage space
+print(f"Part 2: {sorted([v for v in dir_sizes.values() if v >= 30_000_000 - (70_000_000 - dir_sizes['//'])])[0]}")
